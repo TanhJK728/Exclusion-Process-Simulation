68cbfeacce765b7a9c949b7971390f9fd6c58153
Tanh <128472586+TanhJK728@users.noreply.github.com>
2026-01-08 17:36:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from model import ExclusionProcess
-from visualizer import plot_spacetime, plot_density_profile
+from visualizer_static import plot_spacetime, plot_density_profile
+from visualizer_dynamic import save_animation
 
 # General Setting
 L = 100  # Lattice size
@@ -10,12 +11,12 @@ q=0.0
 
 # Several Models
 
-#'''
+'''
 # 1. SEP (Symmetric): p=0.5, q=0.5
 # Particles diffuse randomly.
 sim = ExclusionProcess(L=L, N=N, p=0.5, q=0.5)
 mode = "SEP"
-#'''
+'''
 
 '''
 # 2. TASEP (Totally Asymmetric): p=1.0, q=0.0
@@ -24,12 +25,12 @@ sim = ExclusionProcess(L, N, p=1.0, q=0.0)
 mode = "TASEP"
 '''
 
-'''
+#'''
 # 3. ASEP (Asymmetric): p=0.8, q=0.2
 # Drift to the right, but occasional backstepping.
 sim = ExclusionProcess(L=L, N=N, p=0.8, q=0.2)
 mode = "ASEP"
-'''
+#'''
 
 print(f"Running {mode} Simulation on L={L} with N={N}...")
 
@@ -40,3 +41,7 @@ sim.run(steps=STEPS, save_every=10)
 print("Simulation complete. Generating plots...")
 plot_spacetime(sim.history, title=f"{mode} Space-Time Evolution")
 plot_density_profile(sim.history)
+
+gif_filename = f"{mode}_evolution.gif"
+print(f"Creating GIF for {mode}...")
+save_animation(sim.history, filename=gif_filename, fps=20, interval = 20)
